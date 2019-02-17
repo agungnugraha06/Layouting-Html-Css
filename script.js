6a8f51e45a483a60495d3e8aecd1d7123c002d62
agungnugrah06 <nugrahaace122@gmail.com>
2019-02-17 19:41:23 +0700

    layouting saya

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+	alert('selamat datang..');
+	var lagi = true;
+
+	while( lagi === true) {
+	var nama = prompt('masukan username:');
+	alert('halo' + nama);
+
+	lagi = confirm ('coba lagi?');
+
+	}
+
+	alert('terima kasih');
